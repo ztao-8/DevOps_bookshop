c04c37faf45d4d49052612d60cdd4afe41565088
ztao-8 <143745233+ztao-8@users.noreply.github.com>
2025-03-03 13:46:18 -0800

    change color

--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 //rafce
 const Update = () => {
 
-    const API_URL = "http://localhost:8800" || import.meta.env.VITE_API_URL;
+    const API_URL =  import.meta.env.VITE_API_URL || "http://localhost:8800" ;
 
     const [book, setBook] = useState({
         title: "",
