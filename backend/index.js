2de01233f797aac47a6a3f104d969c0b5d8321be
ztao-8 <143745233+ztao-8@users.noreply.github.com>
2025-03-16 03:51:52 -0700

    feat: force new release

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ const corsOptions = {
     allowedHeaders: ["Content-Type", "Authorization"]
 };
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.get("/", (req,res)=>{
     res.json("Hello World from the backend!!!")
